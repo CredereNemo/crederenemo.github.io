f52bb0ad3dfb0b361bfba446289a433a1de4f6e4
Crederenemo <60203650+CredereNemo@users.noreply.github.com>
2025-04-03 22:59:59 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -4,20 +4,26 @@
     Lampa.Platform.tv();
     
     var server_protocol = location.protocol === "https:" ? 'https://' : 'http://';
-    var icon_server_redirect = '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M13 21.75C13.4142 21.75 13.75 21.4142 13.75 21C13.75 20.5858 13.4142 20.25 13 20.25V21.75ZM3.17157 19.8284L3.7019 19.2981H3.7019L3.17157 19.8284ZM20.8284 4.17157L20.2981 4.7019V4.7019L20.8284 4.17157ZM21.25 13C21.25 13.4142 21.5858 13.75 22 13.75C22.4142 13.75 22.75 13.4142 22.75 13H21.25Z"></path></svg>';
+    var icon_server_redirect = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M13 21.75C13.4142 21.75 13.75 21.4142 13.75 21C13.75 20.5858 13.4142 20.25 13 20.25V21.75ZM3.17157 19.8284L3.7019 19.2981H3.7019L3.17157 19.8284ZM20.8284 4.17157L20.2981 4.7019V4.7019L20.8284 4.17157ZM21.25 13C21.25 13.4142 21.5858 13.75 22 13.75C22.4142 13.75 22.75 13.4142 22.75 13H21.25Z"></path></svg>';
 
     function startRedirectButton() {
+        // Удаляем старую кнопку, если она есть
         $('#REDIRECT').remove();
         
         var servers = Lampa.Storage.get('location_servers') || [];
-        if (!servers.length) return; // Если серверов нет, не создаем кнопку
         
+        // Если серверов нет, не создаем кнопку
+        if (!servers.length) return;
+
+        // Создаем кнопку с иконкой
         var buttonHtml = '<div id="REDIRECT" class="head__action selector redirect-screen">' + icon_server_redirect + '</div>';
         
+        // Добавляем кнопку в нужное место
         $('.head__actions').append(buttonHtml);
         $('#REDIRECT').insertAfter('div[class="head__action selector open--settings"]');
 
-        $('#REDIRECT').on('hover:enter', function() {
+        // Настроим обработчик нажатия на кнопку
+        $('#REDIRECT').on('click', function() {
             openServerSelection();
         });
     }
@@ -35,9 +41,12 @@
             return;
         }
         
-        var options = servers.map(server => ({ title: server, callback: () => {
-            window.location.href = server_protocol + server;
-        }}));
+        var options = servers.map(server => ({
+            title: server,
+            callback: () => {
+                window.location.href = server_protocol + server;
+            }
+        }));
         
         Lampa.Select.show({
             title: 'Выберите сервер',
@@ -46,12 +55,14 @@
         });
     }
 
+    // Добавляем компонент в настройки
     Lampa.SettingsApi.addComponent({
         component: 'location_redirect',
         name: 'Смена сервера',
         icon: icon_server_redirect
     });
 
+    // Настройки параметров для сервера
     Lampa.SettingsApi.addParam({
         component: 'location_redirect',
         param: {
@@ -72,10 +83,14 @@
         }
     });
     
-    if (window.appready) startRedirectButton();
-    else {
+    // Инициализация кнопки
+    if (window.appready) {
+        startRedirectButton();
+    } else {
         Lampa.Listener.follow('app', function(e) {
-            if (e.type === 'ready') startRedirectButton();
+            if (e.type === 'ready') {
+                startRedirectButton();
+            }
         });
     }
 })();
