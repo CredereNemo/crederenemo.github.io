0730c7a1648ec04ededb183dc3633853037377e5
Crederenemo <60203650+CredereNemo@users.noreply.github.com>
2025-04-03 20:44:12 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,19 +2,16 @@
     'use strict';
 
     Lampa.Platform.tv();
-    const server_protocol = location.protocol === "https:" ? 'https://' : 'http://';
 
-    const icon_server_redirect = `
-        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5 4h9a1 1 0 0 1 1 1v5h3V5a3 3 0 0 0-3-3H5a3 3 0 0 0-3 3v5h3V5a1 1 0 0 1 1-1zm14 16h-9a1 1 0 0 1-1-1v-5H6v5a3 3 0 0 0 3 3h9a3 3 0 0 0 3-3v-5h-3v5a1 1 0 0 1-1 1z"/>
-        </svg>`;
+    var server_protocol = location.protocol === "https:" ? 'https://' : 'http://';
+    var icon_server_redirect = '<svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M13 21.75C13.4142 21.75 13.75 21.4142 13.75 21C13.75 20.5858 13.4142 20.25 13 20.25V21.75ZM3.17157 19.8284L3.7019 19.2981H3.7019L3.17157 19.8284ZM20.8284 4.17157L20.2981 4.7019V4.7019L20.8284 4.17157ZM21.25 13C21.25 13.4142 21.5858 13.75 22 13.75C22.4142 13.75 22.75 13.4142 22.75 13H21.25ZM10 3.75H14V2.25H10V3.75ZM2.75 13V12H1.25V13H2.75ZM13 20.25H10V21.75H13V20.25ZM21.25 11V12H22.75V11H21.25ZM1.25 13C1.25 14.8644 1.24841 16.3382 1.40313 17.489C1.56076 18.6614 1.89288 19.6104 2.64124 20.3588L3.7019 19.2981C3.27869 18.8749 3.02502 18.2952 2.88976 17.2892C2.75159 16.2615 2.75 14.9068 2.75 13H1.25ZM10 20.25C8.09318 20.25 6.73851 20.2484 5.71085 20.1102C4.70476 19.975 4.12511 19.7213 3.7019 19.2981L2.64124 20.3588C3.38961 21.1071 4.33855 21.4392 5.51098 21.5969C6.66182 21.7516 8.13558 21.75 10 21.75V20.25Z" /></svg>';
 
     function getServers() {
-        return Lampa.Storage.get('location_servers') || [];
+        return JSON.parse(Lampa.Storage.get('servers') || '[]');
     }
 
     function saveServers(servers) {
-        Lampa.Storage.set('location_servers', servers);
+        Lampa.Storage.set('servers', JSON.stringify(servers));
     }
 
     function checkServerStatus(url, callback) {
@@ -23,36 +20,61 @@
             .catch(() => callback(false));
     }
 
-    function updateRedirectButton() {
-        $('#REDIRECT').remove();
-        const servers = getServers();
+    function selectServer() {
+        let servers = getServers();
 
-        if (servers.length === 0) return;
+        if (servers.length === 1) {
+            window.location.href = server_protocol + servers[0].url;
+            return;
+        }
 
-        const button = `<div id="REDIRECT" class="head__action selector redirect-screen">${icon_server_redirect}</div>`;
-        $('div.head__actions').append(button);
+        let items = servers.map(server => ({
+            title: server.name + (server.status ? ' ✅' : ' ❌'),
+            url: server.url
+        }));
 
-        $('#REDIRECT').on('click hover:enter hover:touch', function () {
-            if (servers.length === 1) {
-                window.location.href = server_protocol + servers[0].url;
-            } else {
-                showServerMenu();
+        Lampa.Select.show({
+            title: 'Выберите сервер',
+            items: items,
+            onSelect: (item) => {
+                window.location.href = server_protocol + item.url;
             }
         });
     }
 
-    function showServerMenu() {
-        const servers = getServers();
-        const items = servers.map(server => ({
-            title: `${server.name} (${server.status ? '🟢' : '🔴'})`,
-            subtitle: server.url,
-            onSelect: () => window.location.href = server_protocol + server.url
-        }));
+    function addServer() {
+        Lampa.Utils.pincode({
+            title: 'Введите название сервера',
+            nofocus: true,
+            onSelect: (name) => {
+                if (!name) return;
 
-        Lampa.Select.show({
-            title: 'Выбор сервера',
-            items: items,
-            onBack: () => Lampa.Controller.toggle('head')
+                Lampa.Utils.pincode({
+                    title: 'Введите адрес сервера',
+                    nofocus: true,
+                    onSelect: (url) => {
+                        if (!url) return;
+
+                        let servers = getServers();
+                        checkServerStatus(server_protocol + url, (status) => {
+                            servers.push({ name, url, status });
+                            saveServers(servers);
+                            updateRedirectButton();
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    function updateRedirectButton() {
+        $('#REDIRECT').remove();
+        let domainBUTT = '<div id="REDIRECT" class="head__action selector redirect-screen">' + icon_server_redirect + '</div>';
+        $('#app > div.head > div > div.head__actions').append(domainBUTT);
+        $('#REDIRECT').insertAfter('div[class="head__action selector open--settings"]');
+
+        $('#REDIRECT').on('hover:enter hover:click hover:touch', function () {
+            selectServer();
         });
     }
 
@@ -65,81 +87,41 @@
     Lampa.SettingsApi.addParam({
         component: 'location_redirect',
         param: {
-            name: 'location_servers',
+            name: 'add_server',
             type: 'button',
-            values: '',
-            placeholder: 'Добавить сервер',
-            default: ''
+            values: 'Добавить сервер'
         },
         field: {
-            name: 'Список серверов',
-            description: 'Нажмите, чтобы управлять списком серверов'
+            name: 'Добавить сервер',
+            description: 'Добавьте новый сервер в список'
         },
-        onChange: function () {
-            showServerManager();
+        onChange: addServer
+    });
+
+    Lampa.SettingsApi.addParam({
+        component: 'location_redirect',
+        param: {
+            name: 'const_redirect',
+            type: 'trigger',
+            default: false
+        },
+        field: {
+            name: 'Постоянный редирект',
+            description: 'Внимание! Если включено, при старте приложения сразу будет загружаться выбранный сервер'
         }
     });
 
-    function showServerManager() {
-        const servers = getServers();
-
-        const items = servers.map((server, index) => ({
-            title: `${server.name} (${server.status ? '🟢' : '🔴'})`,
-            subtitle: server.url,
-            onSelect: () => removeServer(index)
-        }));
-
-        items.push({
-            title: '➕ Добавить сервер',
-            onSelect: () => addServer()
-        });
-
-        Lampa.Select.show({
-            title: 'Управление серверами',
-            items: items,
-            onBack: () => Lampa.Controller.toggle('settings')
-        });
-    }
-
-    function addServer() {
-        Lampa.Input.open({
-            title: 'Добавить сервер',
-            value: '',
-            placeholder: 'Введите название сервера',
-            onEnter: (name) => {
-                Lampa.Input.open({
-                    title: 'Введите адрес сервера',
-                    value: '',
-                    placeholder: 'Например: lampa.surge.sh',
-                    onEnter: (url) => {
-                        const servers = getServers();
-                        checkServerStatus(server_protocol + url, (status) => {
-                            servers.push({ name, url, status });
-                            saveServers(servers);
-                            updateRedirectButton();
-                            showServerManager();
-                        });
-                    }
-                });
-            }
-        });
-    }
-
-    function removeServer(index) {
-        const servers = getServers();
-        servers.splice(index, 1);
-        saveServers(servers);
-        updateRedirectButton();
-        showServerManager();
-    }
-
     if (Lampa.Storage.field('const_redirect')) {
-        const servers = getServers();
-        if (servers.length > 0) window.location.href = server_protocol + servers[0].url;
+        let servers = getServers();
+        if (servers.length > 0) {
+            window.location.href = server_protocol + servers[0].url;
+        }
     }
 
-    Lampa.Listener.follow('app', (e) => {
-        if (e.type === 'ready') updateRedirectButton();
-    });
-
+    if (window.appready) updateRedirectButton();
+    else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type === 'ready') updateRedirectButton();
+        });
+    }
 })();
