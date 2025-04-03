791614f15b0513d63a5fdcb53672328a6a93cfe7
Crederenemo <60203650+CredereNemo@users.noreply.github.com>
2025-04-03 22:14:55 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -4,37 +4,41 @@
     Lampa.Platform.tv();
     
     var server_protocol = location.protocol === "https:" ? 'https://' : 'http://';
-    var icon_server_redirect = '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path d="M13 21.75C13.4142 21.75 13.75 21.4142 13.75 21C13.75 20.5858 13.4142 20.25 13 20.25V21.75ZM3.17157 19.8284L3.7019 19.2981H3.7019L3.17157 19.8284Z"/> </svg>';
+    var icon_server_redirect = '<svg width="24" height="24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M13 21.75C13.4142 21.75 13.75 21.4142 13.75 21C13.75 20.5858 13.4142 20.25 13 20.25V21.75ZM3.17157 19.8284L3.7019 19.2981H3.7019L3.17157 19.8284ZM20.8284 4.17157L20.2981 4.7019V4.7019L20.8284 4.17157ZM21.25 13C21.25 13.4142 21.5858 13.75 22 13.75C22.4142 13.75 22.75 13.4142 22.75 13H21.25ZM10 3.75H14V2.25H10V3.75Z"></path></svg>';
     
     function openServerSelection() {
         var servers = Lampa.Storage.get('location_servers') || [];
-        if (!servers.length) return;
         
-        var list = servers.map(server => {
-            return {
-                title: server,
-                click: () => {
-                    window.location.href = server_protocol + server;
-                }
-            };
-        });
+        if (!Array.isArray(servers) || servers.length === 0) {
+            Lampa.Noty.show('Список серверов пуст');
+            return;
+        }
         
         Lampa.Modal.open({
-            title: 'Выбор сервера',
-            html: '',
-            size: 'medium',
-            buttons: list
+            title: 'Выберите сервер',
+            html: Lampa.Template.js(`
+                <div>
+                    ${servers.map(server => `<div class="server-option selector" data-server="${server}">${server}</div>`).join('')}
+                </div>
+            `),
+            onBack: () => Lampa.Modal.close(),
+        });
+        
+        $('.server-option').on('hover:enter', function() {
+            var selectedServer = $(this).data('server');
+            window.location.href = server_protocol + selectedServer;
         });
     }
     
     function startMe() {
         $('#REDIRECT').remove();
-        var button = '<div id="REDIRECT" class="head__action selector redirect-screen">' + icon_server_redirect + '</div>';
-        $('#app > div.head > div > div.head__actions').append(button);
+        var buttonHTML = `<div id="REDIRECT" class="head__action selector redirect-screen">${icon_server_redirect}</div>`;
+        $('#app > div.head > div > div.head__actions').append(buttonHTML);
+        $('#REDIRECT').insertAfter('div[class="head__action selector open--settings"]');
         
-        $('#REDIRECT').on('hover:enter hover:click hover:touch', openServerSelection);
+        $('#REDIRECT').on('hover:enter', openServerSelection);
     }
-
+    
     Lampa.SettingsApi.addComponent({
         component: 'location_redirect',
         name: 'Смена сервера',
@@ -47,16 +51,15 @@
             name: 'location_servers',
             type: 'input',
             values: '',
-            placeholder: 'Введите сервера через запятую',
+            placeholder: 'Введите список серверов через запятую',
             default: ''
         },
         field: {
             name: 'Список серверов',
-            description: 'Введите адреса серверов через запятую, затем сохраните настройки'
+            description: 'Введите список серверов через запятую, затем нажмите кнопку в верхнем баре'
         },
-        onChange: function(value) {
-            var servers = value.split(',').map(s => s.trim()).filter(s => s);
-            Lampa.Storage.set('location_servers', servers);
+        onChange: function (value) {
+            Lampa.Storage.set('location_servers', value.split(',').map(s => s.trim()));
             startMe();
         }
     });
@@ -64,9 +67,8 @@
     if (window.appready) startMe();
     else {
         Lampa.Listener.follow('app', function(e) {
-            if (e.type == 'ready') {
-                startMe();
-            }
+            if (e.type === 'ready') startMe();
         });
     }
 })();
+
