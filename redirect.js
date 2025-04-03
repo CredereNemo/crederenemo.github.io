0f618ad65727b8ac2c1ee1b47157fa5b094876f5
Crederenemo <60203650+CredereNemo@users.noreply.github.com>
2025-04-03 21:20:57 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -4,39 +4,31 @@
     Lampa.Platform.tv();
 
     var server_protocol = location.protocol === "https:" ? 'https://' : 'http://';
-    var servers = Lampa.Storage.get('servers') || [];
+    var icon_server_redirect = '<svg width="256px" height="256px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M13 21.75C13.4142 21.75 13.75 21.4142 13.75 21C13.75 20.5858 13.4142 20.25 13 20.25V21.75ZM3.17157 19.8284L3.7019 19.2981H3.7019L3.17157 19.8284ZM20.8284 4.17157L20.2981 4.7019V4.7019L20.8284 4.17157ZM21.25 13C21.25 13.4142 21.5858 13.75 22 13.75C22.4142 13.75 22.75 13.4142 22.75 13H21.25ZM10 3.75H14V2.25H10V3.75ZM2.75 13V12H1.25V13H2.75ZM2.75 12V11H1.25V12H2.75ZM13 20.25H10V21.75H13V20.25ZM21.25 11V12H22.75V11H21.25ZM1.25 13C1.25 14.8644 1.24841 16.3382 1.40313 17.489C1.56076 18.6614 1.89288 19.6104 2.64124 20.3588L3.7019 19.2981C3.27869 18.8749 3.02502 18.2952 2.88976 17.2892C2.75159 16.2615 2.75 14.9068 2.75 13H1.25ZM10 20.25C8.09318 20.25 6.73851 20.2484 5.71085 20.1102C4.70476 19.975 4.12511 19.7213 3.7019 19.2981L2.64124 20.3588C3.38961 21.1071 4.33855 21.4392 5.51098 21.5969C6.66182 21.7516 8.13558 21.75 10 21.75V20.25ZM14 3.75C15.9068 3.75 17.2615 3.75159 18.2892 3.88976C19.2952 4.02502 19.8749 4.27869 20.2981 4.7019L21.3588 3.64124C20.6104 2.89288 19.6614 2.56076 18.489 2.40313C17.3382 2.24841 15.8644 2.25 14 2.25V3.75ZM22.75 11C22.75 9.13558 22.7516 7.66182 22.5969 6.51098C22.4392 5.33855 22.1071 4.38961 21.3588 3.64124L20.2981 4.7019C20.7213 5.12511 20.975 5.70476 21.1102 6.71085C21.2484 7.73851 21.25 9.09318 21.25 11H22.75ZM10 2.25C8.13558 2.25 6.66182 2.24841 5.51098 2.40313C4.33856 2.56076 3.38961 2.89288 2.64124 3.64124L3.7019 4.7019C4.12511 4.27869 4.70476 4.02502 5.71085 3.88976C6.73851 3.75159 8.09318 3.75 10 3.75V2.25ZM2.75 11C2.75 9.09318 2.75159 7.73851 2.88976 6.71085C3.02502 5.70476 3.27869 5.12511 3.7019 4.7019L2.64124 3.64124C1.89288 4.38961 1.56076 5.33855 1.40313 6.51098C1.24841 7.66182 1.25 9.13558 1.25 11H2.75Z"></path></svg>';
 
-    function updateIcon() {
+    function startMe() {
         $('#REDIRECT').remove();
-        if (servers.length === 0) return;
-
-        var icon_server_redirect = '<svg width="256px" height="256px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">...</svg>';
         var domainBUTT = '<div id="REDIRECT" class="head__action selector redirect-screen">' + icon_server_redirect + '</div>';
         $('#app > div.head > div > div.head__actions').append(domainBUTT);
         $('#REDIRECT').insertAfter('div[class="head__action selector open--settings"]');
-
+        
+        if (!Lampa.Storage.get('location_server')) {
+            setTimeout(() => $('#REDIRECT').remove(), 10);
+        }
+        
         $('#REDIRECT').on('hover:enter hover:click hover:touch', function() {
+            let servers = Lampa.Storage.get('servers') || [];
             if (servers.length > 1) {
-                showServerList();
-            } else {
+                Lampa.Select.show({
+                    title: 'Выбор сервера',
+                    items: servers.map((s, i) => ({ title: s.name, index: i })),
+                    onSelect: (item) => {
+                        Lampa.Storage.set('location_server', servers[item.index].url);
+                        window.location.href = server_protocol + servers[item.index].url;
+                    }
+                });
+            } else if (servers.length === 1) {
                 window.location.href = server_protocol + servers[0].url;
-            }
-        });
-    }
-
-    function showServerList() {
-        var list = Lampa.Storage.get('servers') || [];
-        var items = list.map((server, index) => ({
-            title: server.name,
-            url: server.url,
-            index: index
-        }));
-
-        Lampa.Select.show({
-            title: 'Выберите сервер',
-            items: items,
-            onSelect: function(item) {
-                window.location.href = server_protocol + item.url;
             }
         });
     }
@@ -44,38 +36,30 @@
     Lampa.SettingsApi.addComponent({
         component: 'location_redirect',
         name: 'Смена сервера',
-        icon: '<svg width="24px" height="24px" fill="currentColor">...</svg>'
+        icon: icon_server_redirect
     });
-
+    
     Lampa.SettingsApi.addParam({
         component: 'location_redirect',
         param: {
-            name: 'add_server',
+            name: 'servers',
             type: 'button',
-            values: 'Добавить сервер'
+            values: '',
+            default: ''
         },
         field: {
-            name: 'Добавить сервер',
-            description: 'Добавить новый сервер с именем и адресом'
+            name: 'Управление серверами',
+            description: 'Добавить или удалить сервер'
         },
-        onChange: function() {
-            Lampa.Modal.input({
-                title: 'Введите имя сервера',
-                onBack: () => Lampa.SettingsApi.show('location_redirect'),
-                onSelect: function(name) {
-                    Lampa.Modal.input({
-                        title: 'Введите адрес сервера',
-                        onBack: () => Lampa.SettingsApi.show('location_redirect'),
-                        onSelect: function(url) {
-                            servers.push({ name, url });
-                            Lampa.Storage.set('servers', servers);
-                            updateIcon();
-                        }
-                    });
-                }
-            });
+        onClick: function () {
+            // Логика добавления сервера
         }
     });
 
-    updateIcon();
+    if (window.appready) startMe();
+    else {
+        Lampa.Listener.follow('app', function(e) {
+            if (e.type == 'ready') startMe();
+        });
+    }
 })();
