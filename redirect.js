208027a8a3a89cab49af96dcff22bafe98096138
Crederenemo <60203650+CredereNemo@users.noreply.github.com>
2025-04-03 21:37:29 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,73 +2,97 @@
     'use strict';
 
     Lampa.Platform.tv();
-    
+
     var server_protocol = location.protocol === "https:" ? 'https://' : 'http://';
-    var servers = Lampa.Storage.get('server_list') || [];
+    var servers = Lampa.Storage.get('server_list', []);
 
-    function updateServerListUI() {
-        var listContainer = $('#server_list');
-        listContainer.empty();
+    function saveServers() {
+        Lampa.Storage.set('server_list', servers);
+    }
 
-        servers.forEach((server, index) => {
-            var item = $('<div class="server-item selector">' + server.name + ' (' + server.address + ')</div>');
-            item.on('hover:enter', function() {
-                Lampa.Storage.set('current_server', server.address);
-                window.location.href = server_protocol + server.address;
-            });
-            listContainer.append(item);
+    function startMe() {
+        $('#REDIRECT').remove();
+        if (servers.length === 0) return;
+
+        var domainSVG = '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M13 21.75C13.4142 21.75 13.75 21.4142 13.75 21C13.75 20.5858 13.4142 20.25 13 20.25V21.75ZM3.17157 19.8284L3.7019 19.2981H3.7019L3.17157 19.8284ZM20.8284 4.17157L20.2981 4.7019V4.7019L20.8284 4.17157ZM21.25 13C21.25 13.4142 21.5858 13.75 22 13.75C22.4142 13.75 22.75 13.4142 22.75 13H21.25Z"></path></svg>';
+        var domainBUTT = '<div id="REDIRECT" class="head__action selector redirect-screen">' + domainSVG + '</div>';
+
+        $('#app > div.head > div > div.head__actions').append(domainBUTT);
+        $('#REDIRECT').insertAfter('div[class="head__action selector open--settings"]');
+
+        $('#REDIRECT').on('hover:enter hover:click hover:touch', function() {
+            if (servers.length > 1) {
+                showServerMenu();
+            } else {
+                window.location.href = server_protocol + servers[0].url;
+            }
         });
     }
 
-    function addServer(name, address) {
-        servers.push({ name: name, address: address });
-        Lampa.Storage.set('server_list', servers);
-        updateServerListUI();
-    }
+    function showServerMenu() {
+        var list = Lampa.Detailed({ title: 'Выбор сервера' });
 
-    function openServerManagement() {
-        var modal = $('<div class="server-management"><h2>Управление серверами</h2></div>');
-        var addButton = $('<button class="add-server">Добавить сервер</button>');
-        var listContainer = $('<div id="server_list"></div>');
-        
-        addButton.on('click', function() {
-            var name = prompt('Введите название сервера');
-            var address = prompt('Введите адрес сервера');
-            if (name && address) {
-                addServer(name, address);
+        servers.forEach(function(server, index) {
+            list.append({
+                title: server.name + ' (' + server.url + ')',
+                onclick: function() {
+                    Lampa.Storage.set('current_server', server.url);
+                    window.location.href = server_protocol + server.url;
+                }
+            });
+        });
+
+        list.append({
+            title: 'Добавить сервер',
+            onclick: function() {
+                addServer();
             }
         });
 
-        modal.append(listContainer);
-        modal.append(addButton);
-        $('body').append(modal);
-        updateServerListUI();
+        Lampa.Activity.push({ component: list });
+    }
+
+    function addServer() {
+        Lampa.Input({ title: 'Добавить сервер', placeholder: 'Введите URL сервера' }, function(url) {
+            if (url) {
+                Lampa.Input({ title: 'Название сервера', placeholder: 'Введите имя' }, function(name) {
+                    servers.push({ name: name || 'Без имени', url: url });
+                    saveServers();
+                    startMe();
+                });
+            }
+        });
     }
 
     Lampa.SettingsApi.addComponent({
-        component: 'location_redirect',
-        name: 'Смена сервера',
-        icon: ''
+        component: 'server_management',
+        name: 'Управление серверами',
+        icon: '<svg width="24" height="24"><circle cx="12" cy="12" r="10" stroke="white" stroke-width="2" fill="none" /></svg>'
     });
 
     Lampa.SettingsApi.addParam({
-        component: 'location_redirect',
+        component: 'server_management',
         param: {
-            name: 'server_management',
-            type: 'button'
+            name: 'server_list',
+            type: 'button',
+            values: '',
+            default: ''
         },
         field: {
-            name: 'Управление серверами',
-            description: 'Добавить или выбрать сервер'
+            name: 'Список серверов',
+            description: 'Просмотр и управление серверами'
         },
-        onChange: openServerManagement
+        onChange: function() {
+            showServerMenu();
+        }
     });
 
-    if (Lampa.Storage.get('current_server')) {
-        var switchButton = $('<div id="REDIRECT" class="head__action selector">Сменить сервер</div>');
-        switchButton.on('hover:enter', function() {
-            openServerManagement();
+    if (window.appready) startMe();
+    else {
+        Lampa.Listener.follow('app', function(e) {
+            if (e.type == 'ready') {
+                startMe();
+            }
         });
-        $('#app > div.head > div > div.head__actions').append(switchButton);
     }
 })();
