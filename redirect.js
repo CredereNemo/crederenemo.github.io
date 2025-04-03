2e3824848cef808374c7b5b5edd390f6b4e41cb2
Crederenemo <60203650+CredereNemo@users.noreply.github.com>
2025-04-03 22:01:22 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,134 +1,60 @@
-(function() {
-    'use strict';
-
-    Lampa.Platform.tv();
-    
-    var server_protocol = location.protocol === "https:" ? 'https://' : 'http://';
-    var icon_server_redirect = '<svg width="256px" height="256px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">...</svg>';
-
-    function startMe() {
-        $('#REDIRECT').remove();
-        
-        if (!Lampa.Storage.get('servers') || Lampa.Storage.get('servers').length === 0) return;
-        
-        var domainSVG = icon_server_redirect;
-        var domainBUTT = '<div id="REDIRECT" class="head__action selector redirect-screen">' + domainSVG + '</div>';
-        
-        $('#app > div.head > div > div.head__actions').append(domainBUTT);
-        $('#REDIRECT').insertAfter('div[class="head__action selector open--settings"]');
-        
-        $('#REDIRECT').on('hover:enter hover:click hover:touch', function() {
-            showServerMenu();
-        });
-    }
-
-    function showServerMenu() {
-        var servers = Lampa.Storage.get('servers') || [];
-        if (servers.length === 0) return;
-
-        var items = servers.map(function(server, index) {
-            return {
-                title: server.name + ' (' + server.address + ')',
-                index: index
-            };
-        });
-
-        Lampa.Select.show({
-            title: 'Выберите сервер',
-            items: items,
-            onSelect: function(item) {
-                var selectedServer = servers[item.index];
-                window.location.href = server_protocol + selectedServer.address;
-            },
-            onBack: function() {
-                Lampa.Controller.back();
+function addServer() {
+    Lampa.Input.edit({
+        value: '',
+        placeholder: 'Введите адрес сервера',
+        onSave: (value) => {
+            if (value.trim() !== '') {
+                let servers = Lampa.Storage.get('custom_servers', '[]');
+                servers = JSON.parse(servers);
+                servers.push(value.trim());
+                Lampa.Storage.set('custom_servers', JSON.stringify(servers));
+                Lampa.Noty.show('Сервер добавлен!');
+            } else {
+                Lampa.Noty.show('Ошибка: пустое значение!');
             }
-        });
-    }
-
-    Lampa.SettingsApi.addComponent({
-        component: 'location_redirect',
-        name: 'Смена сервера',
-        icon: icon_server_redirect
-    });
-
-    Lampa.SettingsApi.addParam({
-        component: 'location_redirect',
-        param: {
-            name: 'servers',
-            type: 'button',
-            values: '',
-            placeholder: 'Управление серверами',
-            default: ''
         },
-        field: {
-            name: 'Список серверов',
-            description: 'Добавьте, удалите или выберите сервер'
-        },
-        onChange: function () {
-            manageServers();
+        onCancel: () => {
+            Lampa.Noty.show('Добавление отменено.');
         }
     });
+}
 
-    function manageServers() {
-        var servers = Lampa.Storage.get('servers') || [];
-        var items = servers.map(function(server, index) {
-            return {
-                title: server.name + ' (' + server.address + ')',
-                index: index,
-                remove: true
-            };
-        });
+function showServerMenu() {
+    let servers = Lampa.Storage.get('custom_servers', '[]');
+    servers = JSON.parse(servers);
 
-        items.push({
-            title: 'Добавить сервер',
-            add: true
-        });
+    let list = servers.map((server, index) => ({
+        title: server,
+        onSelect: () => {
+            Lampa.Noty.show('Выбран сервер: ' + server);
+        },
+        onDelete: () => {
+            servers.splice(index, 1);
+            Lampa.Storage.set('custom_servers', JSON.stringify(servers));
+            Lampa.Noty.show('Сервер удалён!');
+            showServerMenu();
+        }
+    }));
 
-        Lampa.Select.show({
-            title: 'Управление серверами',
-            items: items,
-            onSelect: function(item) {
-                if (item.add) {
-                    addServer();
-                } else {
-                    servers.splice(item.index, 1);
-                    Lampa.Storage.set('servers', servers);
-                    manageServers();
-                }
-            },
-            onBack: function() {
-                Lampa.Controller.back();
-            }
-        });
+    if (list.length === 0) {
+        list.push({ title: 'Список пуст', disabled: true });
     }
 
-    function addServer() {
-        Lampa.Input.show({
-            title: 'Добавить сервер',
-            placeholder: 'Введите имя сервера',
-            onEnter: function(name) {
-                Lampa.Input.show({
-                    title: 'Введите адрес сервера',
-                    placeholder: 'Например: lampa.surge.sh',
-                    onEnter: function(address) {
-                        var servers = Lampa.Storage.get('servers') || [];
-                        servers.push({ name: name, address: address });
-                        Lampa.Storage.set('servers', servers);
-                        manageServers();
-                    }
-                });
-            }
-        });
-    }
+    Lampa.Select.show({
+        title: 'Список серверов',
+        items: list,
+        onSelect: (item) => item.onSelect && item.onSelect(),
+        onBack: () => {
+            Lampa.Controller.toggle('content');
+        }
+    });
+}
 
-    if (window.appready) startMe();
-    else {
-        Lampa.Listener.follow('app', function(e) {
-            if (e.type == 'ready') {
-                startMe();
-            }
-        });
+// Добавляем кнопку в меню
+Lampa.Settings.addParam({
+    title: 'Управление серверами',
+    onSelect: showServerMenu,
+    onBack: () => {
+        Lampa.Controller.toggle('content');
     }
-
-})();
+});
